c0d278a08a61bc71e605de7ad6841400948ffe70
Raiden <xinlei@powere2e.comgit config --global user.email xinlei@powere2e.com>
2019-01-03 19:03:29 +0800

    上传注解

---
+++ src/main/java/core/annotation/MyComponent.java
@@ -0,0 +1,10 @@
+package core.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyComponent {
+    String value() default "";
+}
---
+++ src/main/java/core/annotation/MyService.java
@@ -0,0 +1,11 @@
+package core.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@MyComponent
+public @interface MyService {
+
+}
