8aaea60fb45a5d88b44e8d40873e2dfce11aea0d
Raiden <xinlei@powere2e.comgit config --global user.email xinlei@powere2e.com>
2019-03-04 11:56:59 +0800

    又花了代理类的代码，提升执行速度

--- src/main/java/aspect/AspectInvacationHandler.java
+++ src/main/java/aspect/AspectInvacationHandler.java
@@ -8,9 +8,13 @@ import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 切面实际处理类
@@ -19,19 +23,72 @@ public class AspectInvacationHandler implements MethodInterceptor {
 
     private Object source;
     private Aspect aspect;
-    private List<Method> beforeMethods;
-    private List<Method> afterMethods;
-    private List<Method> aroundMethods;
+    //前置方法映射
+    private Map<String, List<Method>> beforeMethodMapping;
+    //后置方法映射
+    private Map<String, List<Method>> afterMethodMapping;
+    //环绕方法映射
+    private Map<String, List<Method>> aroundMethodMapping;
 
     public AspectInvacationHandler(Object source,Aspect aspect,List<Method> beforeMethods,List<Method> afterMethods,List<Method> aroundMethods){
         super();
         this.source = source;
         this.aspect = aspect;
-        this.beforeMethods = beforeMethods;
-        this.afterMethods = afterMethods;
-        this.aroundMethods = aroundMethods;
+        this.beforeMethodMapping = new HashMap<>();
+        this.afterMethodMapping = new HashMap<>();
+        this.aroundMethodMapping = new HashMap<>();
+        init(beforeMethods, afterMethods, aroundMethods);
     }
 
+    private void init(List<Method> beforeMethods,List<Method> afterMethods,List<Method> aroundMethods){
+        Method[] methods = source.getClass().getDeclaredMethods();
+        for (Method method : methods) {
+            String methodName = method.getName();
+            this.beforeMethodMapping.put(methodName, methodFilter(beforeMethods, methodName));
+            this.afterMethodMapping.put(methodName, methodFilter(afterMethods, methodName));
+            this.aroundMethodMapping.put(methodName, methodFilter(aroundMethods, methodName));
+        }
+
+    }
+
+    /**
+     * 根据被拦截的方法名称，获取需要执行的前置方法或者后置方法
+     * @param methods
+     * @param methodName
+     * @return
+     */
+    private List<Method> methodFilter(List<Method> methods,String methodName){
+        List<Method> result = new ArrayList<>();
+        for (Method method : methods) {
+            if (null != method){
+                if (isIntercept(methodName, getMethodNames(method))){
+                    result.add(method);
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 获取注解中的需要拦截方法名称
+     * @param methon
+     * @return
+     */
+    private String[] getMethodNames(Method methon){
+        if (methon.isAnnotationPresent(MyAfter.class)){
+            return methon.getAnnotation(MyAfter.class).MethodNames();
+        }else if (methon.isAnnotationPresent(MyBefore.class)){
+            return methon.getAnnotation(MyBefore.class).MethodNames();
+        }else if (methon.isAnnotationPresent(MyAround.class)){
+            return methon.getAnnotation(MyAround.class).MethodNames();
+        }
+        return new String[0];
+    }
+
+    /**
+     * 获取一个代理实例
+     * @return
+     */
     public Object getInstance() {
         // 操作字节码 生成虚拟子类
         Enhancer enhancer = new Enhancer();
@@ -41,36 +98,21 @@ public class AspectInvacationHandler implements MethodInterceptor {
     }
 
     private void before(String methodName, Object[] args) throws InvocationTargetException, IllegalAccessException {
-        for (Method m : beforeMethods) {
-            if (null != m){
-                MyBefore before = m.getAnnotation(MyBefore.class);
-                if (isIntercept(methodName, before.MethodNames())){
-                    m.invoke(aspect);
-                }
-            }
+        for (Method m : beforeMethodMapping.get(methodName)) {
+            m.invoke(aspect);
         }
     }
 
     private void after(String methodName, Object[] args) throws InvocationTargetException, IllegalAccessException {
-        for (Method m : afterMethods) {
-            if (null != m){
-                MyAfter after = m.getAnnotation(MyAfter.class);
-                if (isIntercept(methodName, after.MethodNames())){
-                    m.invoke(aspect);
-                }
-            }
+        for (Method m : afterMethodMapping.get(methodName)) {
+            m.invoke(aspect);
         }
     }
 
     private Object around(String methodName, Method method, Object[] args) throws InvocationTargetException, IllegalAccessException {
-        for (Method m : aroundMethods) {
-            if (null != m){
-                MyAround around = m.getAnnotation(MyAround.class);
-                if (isIntercept(methodName, around.MethodNames())){
-                    Object[] objects = {new ProceedingJoinPoint(source, method, args)};
-                    return m.invoke(aspect, objects);
-                }
-            }
+        for (Method m : aroundMethodMapping.get(methodName)) {
+            Object[] objects = {new ProceedingJoinPoint(source, method, args)};
+            return m.invoke(aspect, objects);
         }
         return method.invoke(source, args);
     }
